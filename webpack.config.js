136162d8d3b0f8679cdcd33599b7b165c428f636
Chris Trombley <ctrombley@gmail.com>
2017-02-18 11:17:01 -0800

    adding source maps and improving webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || "3000";
 
 module.exports = {
-  devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
+    'react-hot-loader',
+    './src/index.jsx',
+    './styles/main.scss'
   ],
+  devtool: 'eval-source-map',
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/build/'
+    publicPath: '/',
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
@@ -20,9 +27,34 @@ module.exports = {
         include: path.join(__dirname, 'src')
       },
       {
-        test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
+        test: /\.scss$/, 
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&localIdentName=[local]___[hash:base64:5]!sass?outputStyle=expanded'),
+        exclude: ['node_modules']
       }
     ]
-  }
+  },
+  devServer: {
+    contentBase: "./public",
+    noInfo: true,
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    port: PORT,
+    host: HOST
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin("style.css", {
+      allChunks: true
+    }),
+    new DashboardPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      files: {
+        css: ['style.css'],
+        js: [ "bundle.js"],
+      }
+    })
+  ]
 }
